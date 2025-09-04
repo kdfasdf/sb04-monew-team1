2b16f459ace744a7f40059c737c5b0a88ef04e9e
kdfasdf <96770726+kdfasdf@users.noreply.github.com>
2025-09-04 17:33:57 +0900

    feat(사용자 관리): 유저 에러코드 수정
    - USER_EMAIL_DUPLICATED_EXCEPTION 에서 USER_EMAIL_DUPLICATED로 수정

--- src/main/java/com/codeit/monew/user/exception/UserErrorCode.java
+++ src/main/java/com/codeit/monew/user/exception/UserErrorCode.java
@@ -9,7 +9,7 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum UserErrorCode implements ErrorCode {
 
-  USER_EMAIL_DUPLICATED_EXCEPTION(HttpStatus.BAD_REQUEST.value(), "이메일이 중복되었습니다.");
+  USER_EMAIL_DUPLICATED(HttpStatus.BAD_REQUEST.value(), "이메일이 중복되었습니다.");
 
   private final int status;
   private final String message;
