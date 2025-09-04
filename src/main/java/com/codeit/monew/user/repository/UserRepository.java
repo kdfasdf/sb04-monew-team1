09515254443181406d4bab4ae35ec14d0e837cd8
kdfasdf <96770726+kdfasdf@users.noreply.github.com>
2025-09-04 16:18:40 +0900

    feat(사용자 관리): 사용자 이메일 조회 메서드 추가
    - UserRepository findByEmail 메서드 추가

--- src/main/java/com/codeit/monew/user/repository/UserRepository.java
+++ src/main/java/com/codeit/monew/user/repository/UserRepository.java
@@ -1,9 +1,12 @@
 package com.codeit.monew.user.repository;
 
 import com.codeit.monew.user.entity.User;
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
   boolean existsByEmail(String email);
+
+  Optional<User> findByEmail(String email);
 }
